163e63b5757dff525ff6ca08f0bda698ac3c3a9b
Shaygi <serol@stud.hs-bremen.de>
2024-05-20 00:32:11 +0200

    Update Chapter1.kt

--- app/src/main/java/com/example/verseverwebt/Chapter1.kt
+++ app/src/main/java/com/example/verseverwebt/Chapter1.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,20 +31,50 @@ class Chapter1 : ComponentActivity() {
 fun Chapter1Content() {
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp), // Hier wird ein Abstand von 8dp zwischen den Elementen eingefügt
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         BackToMenuButton()
-        Spacer(modifier = Modifier.height(16.dp))
+
+        Spacer(modifier = Modifier.height(32.dp)) // Hier kannst du die Höhe des Spacers anpassen, um die Lücke zu reduzieren
+
         Text(
-            text = "Chapter 1",
+            text = "CHAPTER",
+            fontFamily = playfair,
             style = MaterialTheme.typography.headlineLarge,
             fontSize = 45.sp,
-            modifier = Modifier.padding(bottom = 16.dp)
+            textAlign = TextAlign.Center
+
         )
-        // Your chapter content here
+
+        Text(
+            text = "One",
+            fontFamily = inspiration,
+            style = MaterialTheme.typography.headlineLarge,
+            fontSize = 45.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 66.dp)
+        )
+        Text(
+            text = "MAN HÖRT GEFLÜSTER",
+            fontFamily = playfair,
+            fontSize = 13.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 26.dp)
+        )
+        Text(
+            text = "Im Rätselmeer verborgen, liegt ein funkelnder Schatz, Klugheit und Weisheit sind der Schlüssel zum Glanz.",
+            fontFamily = playfair,
+            style = MaterialTheme.typography.bodySmall,
+            fontSize = 5.sp,
+            textAlign = TextAlign.Center,
+
+            )
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
