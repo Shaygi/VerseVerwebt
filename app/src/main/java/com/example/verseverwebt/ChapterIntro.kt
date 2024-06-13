12d5a2fea50c17eb934a367a31c54e7d7c960a22
Shaygi <serol@stud.hs-bremen.de>
2024-06-13 21:16:57 +0200

    Update ChapterIntro.kt
    Kommentare im ChapterIntro

--- app/src/main/java/com/example/verseverwebt/ChapterIntro.kt
+++ app/src/main/java/com/example/verseverwebt/ChapterIntro.kt
@@ -19,12 +19,14 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.verseverwebt.ui.theme.VerseVerwebtTheme
 
+//Intro Chapter that is played once after registering for the first time
+//Player needs to turn on the flashlight to solve the riddle
 class ChapterIntro : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //turn flashlight off on create
         turnOffFlashlight()
-
+        //content of the page
         setContent {
             VerseVerwebtTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
@@ -33,38 +35,52 @@ class ChapterIntro : ComponentActivity() {
             }
         }
     }
+
+    //Funktion zum ausschalten der Taschenlampe
     private fun turnOffFlashlight() {
+        // Access to Camera manager
         val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        // sets flashlight mode to false
         cameraManager.setTorchMode(cameraManager.cameraIdList[0], false)
     }
 }
 
 @Composable
 fun ChapterIntroContent() {
+    //Saves status of the flashlight
     var flashlightOn by remember { mutableStateOf(false) }
-
+    //current context
     val context = LocalContext.current
+    //Camera service from current context
     val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
+    // Uses DisposableEffect to free resources if the effect is not used anymore
     DisposableEffect(Unit) {
+        // Creates a TorchCallback instance to react to changes from the flashlight mode
         val torchCallback = object : CameraManager.TorchCallback() {
+            // is called if flashlight mode changes
             override fun onTorchModeChanged(cameraId: String, enabled: Boolean) {
-                flashlightOn = enabled
+                flashlightOn = enabled // Updates the status of the flashlight
             }
         }
-
+        // Registers TorchCallback at CameraManager
         cameraManager.registerTorchCallback(torchCallback, null)
+
+        //frees resources if the effect is not used anymore
         onDispose {
+            // Removes the TorchCallback from CameraManager
             cameraManager.unregisterTorchCallback(torchCallback)
         }
     }
 
     Box(
+
         modifier = Modifier
             .fillMaxSize()
-            .background(if (flashlightOn) Color.Transparent else Color.Black),
+            .background(if (flashlightOn) Color.Transparent else Color.Black), //background color that changes depending on the flashlight status
         contentAlignment = Alignment.Center
     ) {
+        //Menu Button appears when flashlight is on
         if (flashlightOn) {
             Box(
                 modifier = Modifier
@@ -75,10 +91,13 @@ fun ChapterIntroContent() {
             }
         }
         Column(
+            //Column alignment
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            //Title
             Text(
+                //color changes when flashlight is on
                 text = "CHAPTER",
                 fontFamily = playfair,
                 style = MaterialTheme.typography.headlineLarge,
@@ -86,16 +105,19 @@ fun ChapterIntroContent() {
                 textAlign = TextAlign.Center,
                 color = if (flashlightOn) Color.Black else Color.Gray
             )
+            //Subtitle
             Text(
+                //color changes when flashlight is on
                 text = "Intro",
                 fontFamily = inspiration,
                 style = MaterialTheme.typography.headlineLarge,
                 fontSize = 45.sp,
                 textAlign = TextAlign.Center,
-
                 color = if (flashlightOn) Color.Black else Color.Gray
             )
+            //Riddletext
             Text(
+                //text, font size, text alignment and color changes when flashlight is on
                 text = if (flashlightOn) "Das Abenteuer beginnt, das erste Rätsel gelöst, Doch im Dunkeln liegen noch viele verschlüsselt. Setze dein Lesezeichen, verirre dich nicht." else "Bringe Licht ins Dunkle....",
                 fontFamily = playfair,
                 style = MaterialTheme.typography.bodySmall,
@@ -108,10 +130,13 @@ fun ChapterIntroContent() {
     }
 }
 
+//function is for previewing in the IDE
 @Preview(showBackground = true)
 @Composable
 fun ChapterIntroContentPreview() {
+    // Sets the theme for the preview
     VerseVerwebtTheme {
+        // Calls the composable function to be previewed
         ChapterIntroContent()
     }
 }
