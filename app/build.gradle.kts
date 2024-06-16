f143c13162e23bf40c61a1abc8c60ab3f997f6df
val <130545294+val8elster@users.noreply.github.com>
2024-06-16 16:28:34 +0200

    retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.gson)
     implementation(libs.converter.gson)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.retrofit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
