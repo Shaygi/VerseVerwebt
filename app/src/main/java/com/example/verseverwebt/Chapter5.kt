85bf1418a180c1818100b5a43708cc699bebd8cd
snb02 <104787845+snb02@users.noreply.github.com>
2024-06-16 19:41:11 +0200

    Merged Chapter 5 (eigentlich soll es Chapter 7 werden)

--- app/src/main/java/com/example/verseverwebt/Chapter5.kt
+++ app/src/main/java/com/example/verseverwebt/Chapter5.kt
@@ -1,53 +1,130 @@
 package com.example.verseverwebt
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import android.database.ContentObserver
+import android.net.Uri
+import com.example.verseverwebt.ui.theme.VerseVerwebtTheme
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.verseverwebt.ui.theme.VerseVerwebtTheme
+import com.example.verseverwebt.ui.theme.CustomTypography
 
 class Chapter5 : ComponentActivity() {
+
+    //a variable of the type ContentObserver is created here for future use
+    private lateinit var contentObserver: ContentObserver
+
+    // A changeable boolean variable that returns the winner status
+    var hasWin by mutableStateOf(false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //A handler is created, this is connected to the main thread and can thus perceive constant ui updates
+        val handler = Handler(Looper.getMainLooper())
+
+        //The ContentObserver is initialized here and monitors the MediaStore
+        contentObserver = object : ContentObserver(handler) {
+            //This function is executed as soon as the MediaStore changes
+            override fun onChange(newScreenshot: Boolean, uri: Uri?) {
+                //Calls the onChange function of the base class
+                super.onChange(newScreenshot, uri)
+                Log.d("Chapter 5", "Screenshot wurde gemacht ")
+                //Sets the winner booelan true
+                hasWin = true
+            }
+        }
+
+        //Registers the created observer with the resolver and thus receives notifications about changes in the media store
+        contentResolver.registerContentObserver(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            true,
+            contentObserver
+        )
+
+        //This function starts the Chapter5Content function and set the Conetnt for this activity
         setContent {
             VerseVerwebtTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Chapter5Content()
+                    Chapter5Content(hasWin)
                 }
             }
         }
     }
+
+    //destroys and logs off the observer if the activity is destroyed
+    override fun onDestroy() {
+        super.onDestroy()
+        contentResolver.unregisterContentObserver(contentObserver)
+    }
 }
 
+//This function contains the design for this activity
 @Composable
-fun Chapter5Content() {
+fun Chapter5Content(hasWin: Boolean) {
+
+    //Vertical text elements, a menu button and a picture of the treasure
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         BackToMenuButton()
-        Spacer(modifier = Modifier.height(16.dp))
+
+        Spacer(modifier = Modifier.height(32.dp))
+
         Text(
-            text = "Chapter 5",
-            style = MaterialTheme.typography.headlineLarge,
-            fontSize = 45.sp,
-            modifier = Modifier.padding(bottom = 16.dp)
+            text = "CHAPTER",
+            style = CustomTypography.titleLarge,
+            textAlign = TextAlign.Center
         )
-        // Your chapter content here
+
+        Text(
+            text = "Five",
+            style = CustomTypography.titleMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(bottom = 66.dp)
+        )
+        Text(
+            text = "Treasure found, a gleam on the horizon so wide," +
+                    "Cherish the moment before it slips with the tide." +
+                    "Be quick to capture your treasure," +
+                    "Hold it closely as a memory forever," +
+                    "Quickly capture the evidence," +
+                    "So you can keep it in your remembrance.",
+            style = CustomTypography.bodyMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(start = 50.dp, end = 50.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.treasure),
+            contentDescription = "treasure",
+            modifier = Modifier
+                .scale(0.6f)
+        )
+    }
+
+    //When the puzzle has been solved, the Chapter Win function is triggered
+    if(hasWin){
+        Chapter5Win()
     }
 }
 
-@Preview(showBackground = true)
+//This function is triggered as soon as the puzzle has been solved
+// and then starts a success pop-up
 @Composable
-fun Chapter5ContentPreview() {
-    VerseVerwebtTheme {
-        Chapter5Content()
-    }
+fun Chapter5Win() {
+    //TODO: HIER POP-UP & DATENBANK INTEGRATION
 }
