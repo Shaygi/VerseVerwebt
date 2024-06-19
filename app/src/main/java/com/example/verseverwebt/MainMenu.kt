a4ed635d64bfa1c98484db7be46f9726730dfd7b
val <130545294+val8elster@users.noreply.github.com>
2024-06-19 15:16:26 +0200

    modified start button to lead to the next chapter or content menu if all chapters have been played

--- app/src/main/java/com/example/verseverwebt/MainMenu.kt
+++ app/src/main/java/com/example/verseverwebt/MainMenu.kt
@@ -3,6 +3,7 @@ package com.example.verseverwebt
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -17,7 +18,15 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.verseverwebt.api.ApiClient
 import com.example.verseverwebt.ui.theme.VerseVerwebtTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 val inspiration = FontFamily(
     Font(R.font.inspiration_regular)
@@ -76,7 +85,21 @@ class MainMenu : ComponentActivity() {
 
             if (isLoggedIn) {
                 ButtonColumn("-Start-", 18.sp) {
-                    context.startActivity(Intent(context, Chapter1::class.java))
+                    CoroutineScope(Dispatchers.Main).launch {
+                        val next = getNextChapter(context)
+
+                        when(next) {
+                            0 -> context.startActivity(Intent(context, ChapterIntro::class.java))
+                            1 -> context.startActivity(Intent(context, Chapter1::class.java))
+                            2 -> context.startActivity(Intent(context, Chapter2::class.java))
+                            3 -> context.startActivity(Intent(context, Chapter3::class.java))
+                            4 -> context.startActivity(Intent(context, Chapter4::class.java))
+                            5 -> context.startActivity(Intent(context, Chapter5::class.java))
+                            6 -> context.startActivity(Intent(context, Chapter6::class.java))
+                            7 -> context.startActivity(Intent(context, Chapter7::class.java))
+                            else -> context.startActivity(Intent(context, TableOfContents::class.java))
+                        }
+                    }
                 }
 
                 ButtonColumn("Contents", 18.sp) {
@@ -113,6 +136,45 @@ class MainMenu : ComponentActivity() {
             val sharedPreferences = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
             sharedPreferences.getBoolean("is_logged_in", false)
         }
+    }
+
+    private suspend fun getNextChapter(context: Context): Int {
+        val user = getUserId(context)
+        return withContext(Dispatchers.Main) {
+            val nextCh = getIfTime(user, 7)
+            Log.d("MainMenu", "Last: $nextCh")
+            nextCh+1
+        }
+    }
+
+
+    suspend fun getIfTime(user: Long, i: Int): Int = withContext(Dispatchers.IO) {
+        if (i > 0) {
+            val chapterTimeResponse = ApiClient.instance.getChapterTime(user, i).execute()
+            if (chapterTimeResponse.isSuccessful) {
+                val chapterTime = chapterTimeResponse.body()
+                if (chapterTime == 0F) {
+                    return@withContext getIfTime(user, i - 1)
+                } else {
+                    return@withContext i
+                }
+            } else {
+                Log.e("MainMenu", "Error fetching Ch$i")
+            }
+        } else {
+            val introCompletedResponse = ApiClient.instance.getIntroCompleted(user).execute()
+            if (introCompletedResponse.isSuccessful) {
+                val introCompleted = introCompletedResponse.body()
+                return@withContext if (introCompleted == false) {
+                    0
+                } else {
+                    10
+                }
+            } else {
+                Log.e("MainMenu", "Error fetching ChapterIntro")
+            }
+        }
+        100
     }
 
     //function is for previewing in the IDE
