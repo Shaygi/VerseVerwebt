9a5abeaae462b9fadcad968d6372653fdda61924
val <130545294+val8elster@users.noreply.github.com>
2024-06-26 21:55:24 +0200

    changed domain from elster.dev:8787 to mobbackend.elster.dev

--- app/src/main/java/com/example/verseverwebt/api/ApiClient.kt
+++ app/src/main/java/com/example/verseverwebt/api/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
     //private const val BASE_URL = "http://10.0.2.2:8787/" //for local hosting
-    private const val BASE_URL = "https://elster.dev:8787/" //for remote hosting
+    private const val BASE_URL = "https://mobbackend.elster.dev/" //for remote hosting
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
